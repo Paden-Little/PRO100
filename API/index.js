ce4637774fb89b52ce703fa5a8b54d5058fb21ec
Paden-Little <78817620+Paden-Little@users.noreply.github.com>
2023-08-11 18:51:28 -0600

    Installed Jquery for test purpose, got messaging working!
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   API/index.js
    	modified:   API/node_modules/.package-lock.json
    	new file:   API/node_modules/jQuery/.npmignore
    	new file:   API/node_modules/jQuery/LICENSE-MIT
    	new file:   API/node_modules/jQuery/README.md
    	new file:   API/node_modules/jQuery/grunt.js
    	new file:   API/node_modules/jQuery/lib/node-jquery.js
    	new file:   API/node_modules/jQuery/package.json
    	new file:   API/node_modules/jQuery/src/wrapper.js
    	new file:   API/node_modules/jQuery/test/core.js
    	new file:   API/node_modules/jQuery/test/css.js
    	new file:   API/node_modules/jQuery/test/fixtures/core.html
    	new file:   API/node_modules/jQuery/test/fixtures/css.css
    	new file:   API/node_modules/jQuery/test/fixtures/css.html
    	new file:   API/node_modules/jQuery/test/helpers/helper.js
    	new file:   API/node_modules/jQuery/tmp/jquery.js
    	modified:   API/package-lock.json
    	modified:   API/package.json
    	modified:   API/public/index.html

--- API/index.js
+++ API/index.js
@@ -4,40 +4,17 @@ const io = require('socket.io')(server);
 const port = 3000;
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
-app.get('/',  (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.emit('message', { manny: 'hey how are you?'});
-  socket.on('another event', (data) => {
-    console.log(data);
-  });
-});
-
-/*const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 8080;
-
-app.get('/', function(req, res) {
-   res.sendfile('index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('user connected');
-  console.log(socket)
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
+    console.log('user connected');
+    socket.on('message', (msg) => {
+        console.log(`message: ${msg}`);
+        io.emit('message', msg);
+    })
 })
-
-server.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
-
-*/
