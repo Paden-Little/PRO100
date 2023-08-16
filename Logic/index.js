4ff16f918073fc321cc7ec5992a726c39b5e6821
TreeBranch <124843730+TreeBramch@users.noreply.github.com>
2023-08-16 14:27:55 -0600

    Player is set up, the set up funtion is not workin correctly but just needs a little adjustment
    On branch main
    Your branch is behind 'origin/main' by 2 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   Logic/index.js

--- Logic/index.js
+++ Logic/index.js
@@ -2,12 +2,41 @@
 
 const deckFactory = require("./Deck.js");
 
-deck = deckFactory.initCards();
+const beginninghand = 7
 
+player =
+{
+    hand: cards = [],
+    first: false
+}
 
+// console.log(deck)
+// deck.shuffle()
+// console.log(deck)
+// console.log(deck.draw())
+// console.log(deck)
 
-console.log(deck)
-deck.shuffle()
-console.log(deck)
-console.log(deck.draw())
-console.log(deck)
+// player.hand = player.hand + deck.draw()
+// player.hand = player.hand + deck.draw()
+// console.log(player.hand)
+
+john1 = player
+kevin2 = player
+
+john1.first = true
+
+function setup()
+{
+    deck = deckFactory.initCards();
+    deck.shuffle()
+    for (let index = 0; index < beginninghand; index++) {
+        john1.hand += deck.draw()
+        kevin2.hand += deck.draw()
+    }
+}
+
+setup()
+
+console.log(john1.hand[0])
+console.log(kevin2.hand)
+console.log(john1)
