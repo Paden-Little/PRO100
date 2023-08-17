c939babc1449d9b4d58022e015bfd05aeb356da5
TreeBranch <124843730+TreeBramch@users.noreply.github.com>
2023-08-17 10:51:16 -0600

    Finished the draw card method
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Logic/index.js

--- Logic/index.js
+++ Logic/index.js
@@ -1,6 +1,8 @@
 //console.log("Hello world!")
 
 const deckFactory = require("./Deck.js");
+const beginninghand = 7;
+var deck
 
 class Player {
     constructor() {
@@ -14,14 +16,16 @@ const kevin2 = new Player();
 
 john1.first = true;
 
-const deck = deckFactory.initCards();
 
-const beginninghand = 7;
-
+// Set up method, initializes deck and shuffles it
 function setup() 
 {
     deck = deckFactory.initCards();
     deck.shuffle();
+}
+// Gives the players their starting cards
+function giveStartCards()
+{
     for (let index = 0; index < beginninghand; index++) {
         john1.hand.push(deck.draw());
         kevin2.hand.push(deck.draw());
@@ -29,6 +33,27 @@ function setup()
 }
 
 setup();
+giveStartCards()
 
-console.log("John1's hand:", john1.hand);
+
+// drawCard method, takes in a player and gives them a card, if the deck is empty as in it returns a null when we draw from it 
+// it will return false helping us know when it is empty
+function drawCard(player)
+{
+    card = deck.draw()
+   
+    if(card == null)
+    {
+        return false
+    }
+    player.hand.push(card)
+    return true
+}
+// This will check if deck is empty if so it will create a new deck and give the player a card from the new deck
+if(!drawCard(kevin2))
+{
+    setup()
+    drawCard(kevin2)
+}
+
 console.log("Kevin2's hand:", kevin2.hand);
