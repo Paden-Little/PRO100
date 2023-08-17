a9a946aafc116c577cd79530d4031011f195eb1b
TreeBranch <124843730+TreeBramch@users.noreply.github.com>
2023-08-17 10:39:45 -0600

    SetUp() is complete and seems to be working fine
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Logic/index.js

--- Logic/index.js
+++ Logic/index.js
@@ -1,34 +1,34 @@
 //console.log("Hello world!")
 
 const deckFactory = require("./Deck.js");
-deck = deckFactory.initCards();
 
-const beginninghand = 7
-
-player =
-{
-    hand: cards = [],
-    first: false
-}
-
-
-const john1 = Object.assign({}, player);
-const kevin2 = Object.assign({}, player);
-
-john1.first = true
-
-function setup()
-{
-    
-    deck.shuffle()
-    for (let index = 0; index < beginninghand; index++) {
-        john1.hand.push(deck.draw())
-        kevin2.hand.push(deck.draw())
+class Player {
+    constructor() {
+        this.hand = [];
+        this.first = false;
     }
 }
 
-setup()
+const john1 = new Player();
+const kevin2 = new Player();
 
-console.log(john1.hand)
-console.log(kevin2.hand)
-console.log(john1)
+john1.first = true;
+
+const deck = deckFactory.initCards();
+
+const beginninghand = 7;
+
+function setup() 
+{
+    deck = deckFactory.initCards();
+    deck.shuffle();
+    for (let index = 0; index < beginninghand; index++) {
+        john1.hand.push(deck.draw());
+        kevin2.hand.push(deck.draw());
+    }
+}
+
+setup();
+
+console.log("John1's hand:", john1.hand);
+console.log("Kevin2's hand:", kevin2.hand);
