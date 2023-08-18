ab70f4ed1fee99c8083dcf4c28a7055fd60e97e9
EmmaHeaton <71049276+ancientem@users.noreply.github.com>
2023-08-17 19:27:08 -0600

    form setup and js for room submission started

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,1 +1,16 @@
 
+
+function validateForm() {
+  var name = document.forms["gameStartup"]["name"].value;
+  var roomnumber = document.forms["gameStartup"]["roomnumber"].value;
+
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+
+  if (roomnumber == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+}
