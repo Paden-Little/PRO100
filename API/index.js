d6f6819f8dba352773b9623ed3361070b7dfde61
Paden-Little <78817620+Paden-Little@users.noreply.github.com>
2023-08-25 20:11:25 -0600

    Moved frontend file to API/Public Did basic refactoring and connecting of components Message features functional, but not pretty

--- API/index.js
+++ API/index.js
@@ -1,3 +1,42 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+// set up express and middlewares
+const app = express();
+const server = http.createServer(app);
+app.use(express.static('public')); 
+
+// set up socket.io
+const io = socketIO(server);
+
+// handle socket connections
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    // handle joinRoom event
+    socket.on('joinRoom', (roomData) => {
+      console.log(roomData)
+        // roomData should be an object like { username: 'user1', roomCode: '123456' }
+        socket.join(roomData.roomCode, () => {
+            console.log(`${roomData.username} joined room ${roomData.roomCode}`);
+            io.to(roomData.roomCode).emit('userJoined', { username: roomData.username });
+        });
+    });
+
+    //handle messsages
+    socket.on('message', (roomCode, message) => {
+      io.to(roomCode).emit('message', message);
+    });
+
+    // handle disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => console.log('Server is running on port 3000'));
+
 /*const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
@@ -19,7 +58,7 @@ io.on('connection', (socket) => {
     })
 })
 */
-
+/*
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
@@ -42,4 +81,5 @@ io.on('connection', (socket) => {
 
 http.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
-});
+});*/
+
---
+++ API/public/index.js
@@ -0,0 +1,127 @@
+const socket = io.connect('http://localhost:3000');
+let roomCode;
+let username;
+
+function validateForm(name, roomNumber) {
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+
+  if (roomNumber == "") {
+    alert("Roomnumber must be filled out");
+    return false;
+  }
+
+  return true;
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('gameStartup').addEventListener('submit', function(e){
+    e.preventDefault();
+        
+    const insideusername = document.getElementById('name').value;
+    const insideroomCode = document.getElementById('roomNumber').value;
+  
+    if(!validateForm(insideusername, insideroomCode)){
+      return;
+    }
+  
+    roomCode = insideroomCode;
+    username = insideusername;
+
+    //Join the room based on info
+    socket.emit('joinRoom', { username: username, roomCode: roomCode });
+
+    //Drop the info screen
+    const modal = document.querySelector('.modal');
+    modal.classList.add('hide');
+    modal.classList.remove('modal');
+  });
+
+  // Get the form element
+const form = document.querySelector('form');
+
+// Add an event listener to the form submission
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Get the message input value
+  const messageInput = document.getElementById('m');
+  const message = messageInput.value;
+
+  // Emit the message event via the socket connection
+  socket.emit('message', roomCode, message);
+
+  // Clear the message input
+  messageInput.value = '';
+
+  return false;
+});
+
+  
+
+socket.on('message', function(msg) {
+  var messages = document.getElementById('messages');
+  var li = document.createElement('li');
+  li.textContent = msg;
+  messages.appendChild(li);
+});
+
+});
+
+
+/* Scroll not currently functional
+const scrollContainer = document.querySelector('.scroll-container');
+let isScrolling = false;
+
+scrollContainer.addEventListener('scroll', () => {
+  if (!isScrolling) {
+    window.requestAnimationFrame(() => {
+      isScrolling = true;
+      // Your scrolling logic here
+      isScrolling = false;
+    });
+  }
+});
+*/
+
+
+/*
+const radioForm = document.getElementById('spinnerForm');
+const radioInputs = document.querySelectorAll('input[type="radio"]');
+const chooseColorButton = document.querySelector('button[type="submit"]');
+
+//this will take in the color choice and make the 'spinner' display none
+//after submition
+
+
+radioForm.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  
+  let selectedValue = null;
+
+  //this will take in the color value
+  radioInputs.forEach(input => {
+    if (input.checked) {
+      selectedValue = input.value;
+    }
+  });
+
+  if (selectedValue !== null) {
+    // Game logic to chose new color
+    console.log('Selected value:', selectedValue);
+    radioForm.style.display = 'none';
+
+  } else {
+    console.log('No option selected');
+  }
+});*/
+
+
+//the submit needs to simultaneously remove the login screen and 
+//send in request to enter room
+//connect cards to logic/json info to display as gameplay runs
+//get cards and create assets for each one
+//leaderboard is hardcoded till we can setup file i/o or database
